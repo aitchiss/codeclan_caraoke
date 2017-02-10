7a9aa1cfc9c33edfe784d8223df4842cd3f0530e
Suzanne <suzanne.aitchison@gmail.com>
2017-02-10 18:37:31 +0000

    Added task_selector to viewer and added to codeclan_karaoke run method

--- codeclan_karaoke.rb
+++ codeclan_karaoke.rb
@@ -1,24 +1,31 @@
+require_relative ('./viewer.rb')
+require_relative ('./karaokebar.rb')
+require_relative ('./room.rb')
+
 class CodeClanCaraoke
 
-  def initialize(viewer)
+  def initialize
 
-    @viewer = viewer
+    @viewer = Viewer.new
 
     room1 = Room.new(5)
     room2 = Room.new(8)
     room3 = Room.new(10)
     room4 = Room.new(20)
 
-    rooms = [room1, room2, room3, room4]
+    @rooms = [room1, room2, room3, room4]
 
-    @codeclancaraoke = KaraokeBar.new("CodeClanCaraoke", rooms, 300)
+    @codeclancaraoke = KaraokeBar.new("CodeClanCaraoke", @rooms, 300)
 
   end
 
   def run
     @viewer.welcome
-    
+    @viewer.task_selector
 
   end
 
 end
+
+karaokebar = CodeClanCaraoke.new
+karaokebar.run
--- viewer.rb
+++ viewer.rb
@@ -1,7 +1,25 @@
+
+
 class Viewer
 
   def welcome
-    puts "Welcome to #{@codeclankaraoke.get_name}!"
+    puts "Welcome!\n**********************************\n"
+  end
+
+  def task_selector
+    puts "What would you like to do? Select from the number options below:"
+    puts "1. Admit new guest"
+    puts "2. Check guest into room"
+    puts "3. Check guest out of room"
+    puts "4. Add song to room playlist"
+    puts "5. Quit the application"
+    @user_input = gets.chomp
+  end
+
+  def get_guest_name
+    puts "Please enter guest name:"
+    @guest_name = gets.chomp
+    return @guest_name
   end
 
 end
