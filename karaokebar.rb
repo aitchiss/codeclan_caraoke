3b7ad8db38c6aa3a89e1380645764e3831925d2b
Suzanne <suzanne.aitchison@gmail.com>
2017-02-10 15:51:34 +0000

    changed KaraokeBar init method to create array of rooms on initializing

--- karaokebar.rb
+++ karaokebar.rb
@@ -1,14 +1,19 @@
 class KaraokeBar
 
-  def initialize(name)
+  def initialize(name, rooms)
 
     @name = name
+    @rooms = rooms
 
   end
 
   def get_name
-
     return @name
-
   end
+
+  def number_of_rooms
+    return @rooms.count
+  end
+
+
 end
--- specs/karaokebar_spec.rb
+++ specs/karaokebar_spec.rb
@@ -1,11 +1,20 @@
 require ('minitest/autorun')
 require_relative ('../karaokebar.rb')
+require_relative ('../room.rb')
 
 class TestKaraokeBar < MiniTest::Test
 
   def setup
 
-    @codeclancaraoke = KaraokeBar.new("CodeClan Caraoke")
+    room_1 = Room.new(12)
+    room_2 = Room.new(18)
+    room_3 = Room.new(5)
+    room_4 = Room.new(10)
+
+    rooms = [room_1, room_2, room_3, room_4]
+
+
+    @codeclancaraoke = KaraokeBar.new("CodeClan Caraoke", rooms)
 
   end
 
@@ -13,6 +22,8 @@ class TestKaraokeBar < MiniTest::Test
     assert_equal("CodeClan Caraoke", @codeclancaraoke.get_name)
   end
 
-  
+  def test_karaoke_bar_initializes_with_rooms
+    assert_equal(4, @codeclancaraoke.number_of_rooms)
+  end
 
 end
