9220be92c5f8e8114ed73c9373256a638d2e6f6f
Suzanne <suzanne.aitchison@gmail.com>
2017-02-10 17:50:26 +0000

    Added files for codeclan karaoke runner and runner spec

---
+++ specs/codeclan_karaoke_spec.rb
@@ -0,0 +1,10 @@
+require ('minitest/autorun')
+require_relative ('../karaokebar.rb')
+require_relative ('../room.rb')
+require_relative ('../guest.rb')
+require_relative ('../song.rb')
+
+class TestCodeClanKaraoke < MiniTest::Test
+
+
+end
